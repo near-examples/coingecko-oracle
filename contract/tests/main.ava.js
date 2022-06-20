2143eb583f5d9a50221b37ffbb411e2950690f32
Dennis <10233439+idea404@users.noreply.github.com>
2022-06-20 09:59:46 +0100

    test single test run

--- contract/tests/main.ava.js
+++ contract/tests/main.ava.js
@@ -82,138 +82,138 @@ test("adding data from wrong account", async (t) => {
   );
 });
 
-test("adding data", async (t) => {
-  const { root, jsvm, contract } = t.context.accounts;
+// test("adding data", async (t) => {
+//   const { root, jsvm, contract } = t.context.accounts;
 
-  await root.call(
-    jsvm,
-    "call_js_contract",
-    encodeCall(contract.accountId, "addPrices", {
-      data : {
-        "1969-12-31T23:03:45.000Z": 111.11,
-        "1970-01-01T23:03:45.000Z": 116.11,
-        "1970-01-02T23:03:45.000Z": 126.11,
-      },
-    }),
-    { attachedDeposit: "400000000000000000000000" }
-  );
+//   await root.call(
+//     jsvm,
+//     "call_js_contract",
+//     encodeCall(contract.accountId, "addPrices", {
+//       data : {
+//         "1969-12-31T23:03:45.000Z": 111.11,
+//         "1970-01-01T23:03:45.000Z": 116.11,
+//         "1970-01-02T23:03:45.000Z": 126.11,
+//       },
+//     }),
+//     { attachedDeposit: "400000000000000000000000" }
+//   );
 
-  t.log(root.accountId);
+//   t.log(root.accountId);
 
-  const result = await jsvm.view(
-    "view_js_contract",
-    encodeCall(contract.accountId, "getPrices", {})
-  );
-  t.log(result);
+//   const result = await jsvm.view(
+//     "view_js_contract",
+//     encodeCall(contract.accountId, "getPrices", {})
+//   );
+//   t.log(result);
 
-  const expected = {
-    '1969-12-31T23:03:45.000Z': 111.11,
-    '1970-01-01T23:03:45.000Z': 116.11,
-    '1970-01-02T23:03:45.000Z': 126.11,
-  };
-  t.deepEqual(result, expected);
-});
+//   const expected = {
+//     '1969-12-31T23:03:45.000Z': 111.11,
+//     '1970-01-01T23:03:45.000Z': 116.11,
+//     '1970-01-02T23:03:45.000Z': 126.11,
+//   };
+//   t.deepEqual(result, expected);
+// });
 
-test("adding data twice", async (t) => {
-  const { root, jsvm, contract } = t.context.accounts;
+// test("adding data twice", async (t) => {
+//   const { root, jsvm, contract } = t.context.accounts;
 
-  await root.call(
-    jsvm,
-    "call_js_contract",
-    encodeCall(contract.accountId, "addPrices", {
-      data : {
-        "1969-12-31T23:03:45.000Z": 111.11,
-        "1970-01-01T23:03:45.000Z": 116.11,
-      },
-    }),
-    { attachedDeposit: "400000000000000000000000" }
-  );
+//   await root.call(
+//     jsvm,
+//     "call_js_contract",
+//     encodeCall(contract.accountId, "addPrices", {
+//       data : {
+//         "1969-12-31T23:03:45.000Z": 111.11,
+//         "1970-01-01T23:03:45.000Z": 116.11,
+//       },
+//     }),
+//     { attachedDeposit: "400000000000000000000000" }
+//   );
 
-  await root.call(
-    jsvm,
-    "call_js_contract",
-    encodeCall(contract.accountId, "addPrices", {
-      data : {
-        "1970-01-02T23:03:45.000Z": 126.11,
-        "1970-01-03T23:03:45.000Z": 136.11,
-      },
-    }),
-    { attachedDeposit: "400000000000000000000000" }
-  );
+//   await root.call(
+//     jsvm,
+//     "call_js_contract",
+//     encodeCall(contract.accountId, "addPrices", {
+//       data : {
+//         "1970-01-02T23:03:45.000Z": 126.11,
+//         "1970-01-03T23:03:45.000Z": 136.11,
+//       },
+//     }),
+//     { attachedDeposit: "400000000000000000000000" }
+//   );
 
-  const result = await jsvm.view(
-    "view_js_contract",
-    encodeCall(contract.accountId, "getPrices", {})
-  );
+//   const result = await jsvm.view(
+//     "view_js_contract",
+//     encodeCall(contract.accountId, "getPrices", {})
+//   );
 
-  const expected = {
-    '1969-12-31T23:03:45.000Z': 111.11,
-    '1970-01-01T23:03:45.000Z': 116.11,
-    '1970-01-02T23:03:45.000Z': 126.11,
-    "1970-01-03T23:03:45.000Z": 136.11,
-  };
-  t.deepEqual(result, expected);
-});
+//   const expected = {
+//     '1969-12-31T23:03:45.000Z': 111.11,
+//     '1970-01-01T23:03:45.000Z': 116.11,
+//     '1970-01-02T23:03:45.000Z': 126.11,
+//     "1970-01-03T23:03:45.000Z": 136.11,
+//   };
+//   t.deepEqual(result, expected);
+// });
 
-test("adding data for existing timestamp", async (t) => {
-  const { root, jsvm, contract } = t.context.accounts;
+// test("adding data for existing timestamp", async (t) => {
+//   const { root, jsvm, contract } = t.context.accounts;
 
-  await root.call(
-    jsvm,
-    "call_js_contract",
-    encodeCall(contract.accountId, "addPrices", {
-      data : {
-        "1969-12-31T23:03:45.000Z": 111.11,
-      },
-    }),
-    { attachedDeposit: "400000000000000000000000" }
-  );
+//   await root.call(
+//     jsvm,
+//     "call_js_contract",
+//     encodeCall(contract.accountId, "addPrices", {
+//       data : {
+//         "1969-12-31T23:03:45.000Z": 111.11,
+//       },
+//     }),
+//     { attachedDeposit: "400000000000000000000000" }
+//   );
 
-  await root.call(
-    jsvm,
-    "call_js_contract",
-    encodeCall(contract.accountId, "addPrices", {
-      data : {
-        "1969-12-31T23:03:45.000Z": 131.11,
-      },
-    }),
-    { attachedDeposit: "400000000000000000000000" }
-  );
+//   await root.call(
+//     jsvm,
+//     "call_js_contract",
+//     encodeCall(contract.accountId, "addPrices", {
+//       data : {
+//         "1969-12-31T23:03:45.000Z": 131.11,
+//       },
+//     }),
+//     { attachedDeposit: "400000000000000000000000" }
+//   );
 
-  const result = await jsvm.view(
-    "view_js_contract",
-    encodeCall(contract.accountId, "getPrices", {})
-  );
+//   const result = await jsvm.view(
+//     "view_js_contract",
+//     encodeCall(contract.accountId, "getPrices", {})
+//   );
 
-  const expected = {
-    "1969-12-31T23:03:45.000Z": 131.11,
-  };
-  t.deepEqual(result, expected);
-});
+//   const expected = {
+//     "1969-12-31T23:03:45.000Z": 131.11,
+//   };
+//   t.deepEqual(result, expected);
+// });
 
-test("adding large numbers", async (t) => {
-  const { root, jsvm, contract } = t.context.accounts;
+// test("adding large numbers", async (t) => {
+//   const { root, jsvm, contract } = t.context.accounts;
 
-  const large_number = 2384762348723648237462231312321113.21321312313121231546431231122314474;
+//   const large_number = 2384762348723648237462231312321113.21321312313121231546431231122314474;
 
-  await root.call(
-    jsvm,
-    "call_js_contract",
-    encodeCall(contract.accountId, "addPrices", {
-      data : {
-        "1969-12-31T23:03:45.000Z": large_number,
-      },
-    }),
-    { attachedDeposit: "400000000000000000000000" }
-  );
+//   await root.call(
+//     jsvm,
+//     "call_js_contract",
+//     encodeCall(contract.accountId, "addPrices", {
+//       data : {
+//         "1969-12-31T23:03:45.000Z": large_number,
+//       },
+//     }),
+//     { attachedDeposit: "400000000000000000000000" }
+//   );
 
-  const result = await jsvm.view(
-    "view_js_contract",
-    encodeCall(contract.accountId, "getPrices", {})
-  );
+//   const result = await jsvm.view(
+//     "view_js_contract",
+//     encodeCall(contract.accountId, "getPrices", {})
+//   );
 
-  const expected = {
-    "1969-12-31T23:03:45.000Z": large_number,
-  };
-  t.deepEqual(result, expected);
-});
+//   const expected = {
+//     "1969-12-31T23:03:45.000Z": large_number,
+//   };
+//   t.deepEqual(result, expected);
+// });
