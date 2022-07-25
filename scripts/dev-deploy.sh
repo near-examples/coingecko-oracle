636d890e5dba62fe1a4b0cfe2647c214efd8245e
Dennis <10233439+idea404@users.noreply.github.com>
2022-07-25 09:56:23 +0000

    update contract wasm names

--- scripts/dev-deploy.sh
+++ scripts/dev-deploy.sh
@@ -1,3 +1,3 @@
 # should be called like this: ./dev-deploy.sh <new-account-name> <master-account-name>
 near create-account $1 --masterAccount $2 --initialBalance 10
-near deploy --accountId $1 --wasmFile contract/build/main.wasm --initFunction init --initArgs '{}'
+near deploy --accountId $1 --wasmFile contract/build/contract.wasm --initFunction init --initArgs '{}'
--- scripts/oracle_deploy.sh
+++ scripts/oracle_deploy.sh
@@ -1,4 +1,4 @@
 # called from project root with: sh scripts/deploy.sh
 near delete oracle.idea404.testnet idea404.testnet
 near create-account oracle.idea404.testnet --masterAccount idea404.testnet --initialBalance 10
-near deploy --accountId oracle.idea404.testnet --wasmFile contract/build/main.wasm --initFunction init --initArgs '{}'
+near deploy --accountId oracle.idea404.testnet --wasmFile contract/build/contract.wasm --initFunction init --initArgs '{}'
